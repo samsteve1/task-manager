59e3ac8e2af279cef82dd3b89f737ddac22ee223
Steve Idowu <idowusteve@ymail.com>
2019-05-03 03:20:37 +0100

    Database Schema and PDO connection

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
+use Todo\Models\Task;
+
 require 'vendor/autoload.php';
+
+try {
+    $db = new PDO('mysql:host=127.0.0.1;dbname=todo', 'root', '');
+
+} catch (PDOException $e) {
+    
+    die($e->getMessage());
+}
+
+
